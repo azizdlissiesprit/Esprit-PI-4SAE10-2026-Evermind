48d77e1c25bd954d9696ad37315fb980b2876d16
coola <aziz.dlissi@esprit.tn>
2026-04-30 11:03:30 +0100

    fixing tests alert ci 2

--- Backend/Alert/Alert/src/test/java/tn/esprit/alert/AlertApplicationTests.java
+++ Backend/Alert/Alert/src/test/java/tn/esprit/alert/AlertApplicationTests.java
@@ -3,7 +3,7 @@ package tn.esprit.alert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Disabled;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 class AlertApplicationTests {
 
     @Test
