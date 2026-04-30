772ad7deebeea7e746d39d09f69f82511ee58966
coola <aziz.dlissi@esprit.tn>
2026-04-30 10:48:21 +0100

    separated ci and cd for alert - imports missing

--- Backend/Alert/Alert/src/test/java/tn/esprit/alert/AlertApplicationTests.java
+++ Backend/Alert/Alert/src/test/java/tn/esprit/alert/AlertApplicationTests.java
@@ -2,7 +2,7 @@ package tn.esprit.alert;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.junit.jupiter.api.Disabled;
 @SpringBootTest
 class AlertApplicationTests {
 
